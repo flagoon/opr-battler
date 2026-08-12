00396a49bd887f643f3b75448fa2f18c0fbac66a
Pawel Kochanek <pawel.kochanek@gmail.com>
2026-08-12 20:02:12 +0200

    add fastapi

--- src/opr_battler/__init__.py
+++ src/opr_battler/__init__.py
@@ -1,2 +1,11 @@
-def main() -> None:
-    print("Hello from opr-battler!")
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", include_in_schema=False)
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html", {"data": "zupa"})
