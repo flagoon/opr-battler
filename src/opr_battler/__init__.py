124a730a74ed631cf505bdda142a45cf2a01bee1
Pawel Kochanek <pawel.kochanek@gmail.com>
2026-08-15 19:53:25 +0200

    Add tailwindcss to jinja2

--- src/opr_battler/__init__.py
+++ src/opr_battler/__init__.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.add_middleware(GZipMiddleware)
 
 templates = Jinja2Templates(directory="templates")
 
