2ec3f945939a7e9421ec3f428c933a352ced593a
Pawel Kochanek <pawel.kochanek@gmail.com>
2026-08-23 21:10:52 +0200

    fix with armies

--- src/opr_battler/__init__.py
+++ src/opr_battler/__init__.py
@@ -3,7 +3,7 @@ from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from opr_battler.army_book import map_all_armies
+from opr_battler.army_book import ArmyBook
 from opr_battler.constants.mocks import armies
 from opr_battler.get_armies import get_armies
 
@@ -14,9 +14,14 @@ app.add_middleware(GZipMiddleware)
 templates = Jinja2Templates(directory="templates")
 
 
+def handle_armies(armies: list[ArmyBook]):
+    return armies
+
+
 @app.get("/", include_in_schema=False, name="home")
 def home(request: Request):
-    return templates.TemplateResponse(request, "home.html", {"armies": armies})
+    handled_armies = handle_armies(armies)
+    return templates.TemplateResponse(request, "home.html", {"armies": handled_armies})
 
 
 @app.get("/get_armies", name="get_armies")
@@ -25,11 +30,5 @@ def army_list():
     Call get armies and returns list of armies for Grimdark Future from OPR page
     """
     r = get_armies()
-    mapped_armies = list(map_all_armies(r))
 
-    with open("src/opr_battler/files/example.csv", "w") as file:
-        file.writelines(
-            f"{army['uid']}; {army['name']}; {army['desc']}; {army['cover_image']}\n"
-            for army in mapped_armies
-        )
-    return mapped_armies
+    return r
--- src/opr_battler/army_book.py
+++ src/opr_battler/army_book.py
@@ -6,7 +6,7 @@ class ArmyBook(TypedDict):
     enabledGameSystems: list[int]
     name: str
     genericName: str
-    hint: str
+    hint: str | None
     background: str
     unitCount: int
     modifiedAt: str
@@ -14,7 +14,7 @@ class ArmyBook(TypedDict):
     official: bool
     versionString: str
     coverImagePath: str
-    bannerImagePath: str
+    bannerImagePath: str | None
     visibility: int
     factionId: str | None
     factionName: str | None
@@ -26,21 +26,3 @@ class ArmyBook(TypedDict):
     popularity: int
     downvotes: int
     flavouredUid: str
-
-
-def map_army(army: ArmyBook):
-    return {
-        "uid": army["uid"],
-        "name": army["name"],
-        "desc": army["background"],
-        "cover_image": army["coverImagePath"],
-    }
-
-
-def map_all_armies(armies: list[ArmyBook]):
-    """
-    Get list of armies from backend, and get only important data from it
-    """
-    mapped_army = map(map_army, armies)
-
-    return mapped_army
