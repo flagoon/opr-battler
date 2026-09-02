903d9f0b01c7d3d8c92eb9977b28546c9a34bd50
Pawel Kochanek <pawel.kochanek@gmail.com>
2026-09-02 19:08:40 +0200

    add icons

--- src/opr_battler/__init__.py
+++ src/opr_battler/__init__.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+
 from fastapi import FastAPI, Request
 from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
 
 from opr_battler.constants.mocks import armies
 from opr_battler.get_armies import get_armies
@@ -16,8 +19,14 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", include_in_schema=False, name="home")
 def home(request: Request):
+    icon_down = Path("static/icons/arrow-down.svg")
+
+    if not icon_down.is_file():
+        print("not a file")
     handled_armies = handle_armies(armies)
-    return templates.TemplateResponse(request, "home.html", {"armies": handled_armies})
+    return templates.TemplateResponse(
+        request, "home.html", {"armies": handled_armies, "icon_down": icon_down}
+    )
 
 
 @app.get("/get_armies", name="get_armies")
